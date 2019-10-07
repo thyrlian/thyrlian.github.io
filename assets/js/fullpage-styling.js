1688e2b070dfe63e5d05bbd7082df0d421fab95b
Jing Li <thyrlian@gmail.com>
2019-10-07 09:02:07 +0200

    Add fullPage license key

--- assets/js/fullpage-styling.js
+++ assets/js/fullpage-styling.js
@@ -1,6 +1,9 @@
+import { getFullPageLicenseKey } from './modules/LicenseKey.js';
+
 const style = getComputedStyle(document.documentElement);
 
 var myFullpage = new fullpage('#fullpage', {
+  licenseKey: getFullPageLicenseKey(),
   anchors: ['intro', 'portfolio', 'contact'],
   sectionsColor: [style.getPropertyValue('--color-turquoise'), style.getPropertyValue('--color-yellow-orange'), style.getPropertyValue('--color-sunset-orange')],
   navigation: true,
---
+++ assets/js/modules/LicenseKey.js
@@ -0,0 +1,3 @@
+export function getFullPageLicenseKey() {
+  return '13A627C6-65A14806-89546D01-D7F348DF';
+}
